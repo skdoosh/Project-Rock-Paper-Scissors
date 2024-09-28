3bdae982edef31a82fd33086e7bd38982b92df37
Shreyas KD <149157430+skdoosh@users.noreply.github.com>
2024-09-28 08:07:36 +0530

    Completed Game

--- script.js
+++ script.js
@@ -5,37 +5,36 @@
 //     map 1, 2, 3 to rock, paper, scissors
 //     return the choice corresponding to the random number
 // write code to get human's choice
-    // Prompt user to enter rock, paper, or scissors
-        // user input validation?
-    // return choice
+// Prompt user to enter rock, paper, or scissors
+// user input validation?
+// return choice
 // write playround function which takes user choice and computer choice as arguments and then console.logs who won and increments the score counters accordingly
 // wrap everything in the play game function which plays 5 rounds of the game.
 // make sure everything works at each step
 
-    // Initialize Scores
-    let humanScore = 0;
-    let computerScore = 0;
+// Initialize Scores
 
-function getComputerChoice() {
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function getComputerChoice() {
     // returns rock, paper or scissors based on randomly generated numbers
-    let randomNumber = Math.floor((Math.random()*3)+1);
-    if (randomNumber == 1)
-        return "rock";
-    else if (randomNumber == 2)
-        return "paper";
-    else if (randomNumber == 3)
-        return "scissors"
-}
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    if (randomNumber == 1) return "rock";
+    else if (randomNumber == 2) return "paper";
+    else if (randomNumber == 3) return "scissors";
+  }
 
-function getHumanChoice(){
+  function getHumanChoice() {
     // returns rock, paper, scissors based on user input
     return prompt(`Enter one of the following:
         Rock
         Paper
         Scissors`);
-}
+  }
 
-function playRound() {
+  function playRound() {
     let humanChoice = getHumanChoice();
     let computerChoice = getComputerChoice();
 
@@ -43,106 +42,100 @@ function playRound() {
     console.log("You have chosen: " + humanChoice + ".");
     console.log("The computer has chosen: " + computerChoice + ".");
     switch (humanChoice) {
-        case 'rock':
-            switch(computerChoice){
-                case 'rock':
-                    tie();
-                    break;
-                case 'paper':
-                    computerWins()
-                    break;
-                case 'scissors':
-                    humanWins();
-                    break;
-                
-                }
+      case "rock":
+        switch (computerChoice) {
+          case "rock":
+            tie();
             break;
-        case 'paper':
-            switch(computerChoice){
-                case 'rock':
-                    humanWins();
-                    break;
-                case 'paper':
-                    tie(); 
-                    break;
-                case 'scissors':
-                    computerWins();
-                    break;
-            }
+          case "paper":
+            computerWins();
             break;
-        case 'scissors':
-            switch(computerChoice){
-                case 'rock':
-                    computerWins()
-                    break;
-                case 'paper':
-                    humanWins();
-                    break;
-                case 'scissors':
-                    tie();
-                    break; 
-            }
-            break;
-        default:
-            console.log("You didn't enter one of the options! Try again!");
-            playRound();
+          case "scissors":
+            humanWins();
             break;
         }
+        break;
+      case "paper":
+        switch (computerChoice) {
+          case "rock":
+            humanWins();
+            break;
+          case "paper":
+            tie();
+            break;
+          case "scissors":
+            computerWins();
+            break;
+        }
+        break;
+      case "scissors":
+        switch (computerChoice) {
+          case "rock":
+            computerWins();
+            break;
+          case "paper":
+            humanWins();
+            break;
+          case "scissors":
+            tie();
+            break;
+        }
+        break;
+      default:
+        console.log("You didn't enter one of the options! Try again!");
+        playRound();
+        break;
+    }
     return;
-}
+  }
 
-function humanWins() {
+  function humanWins() {
     console.log("You win!");
-    humanScore+=1;
+    humanScore += 1;
     displayScore();
     return;
-}
+  }
 
-function computerWins() {
+  function computerWins() {
     console.log("Computer wins!");
-    computerScore+=1;
+    computerScore += 1;
     displayScore();
     return;
-}
+  }
 
-function tie() {
-    console.log("It's a tie!")
+  function tie() {
+    console.log("It's a tie!");
     displayScore();
     return;
-}
+  }
 
-function displayScore() {
+  function displayScore() {
     console.log(`Scoreboard
         Your Score: ${humanScore}
-        Computer Score: ${computerScore}`)
-}
+        Computer Score: ${computerScore}`);
+  }
 
-function evalScore() {
+  function evalScore() {
     console.log("\n");
-    if (humanScore > computerScore){
-        console.log("You Win the Game!");
-    }
-    else if (computerScore > humanScore){
-        console.log("Computer Wins the Game!");
+    if (humanScore > computerScore) {
+      console.log("You Win the Game!");
+    } else if (computerScore > humanScore) {
+      console.log("Computer Wins the Game!");
     } else {
-        console.log("It's a tie.")
+      console.log("It's a tie.");
     }
+  }
+
+  humanScore = 0;
+  computerScore = 0;
+
+  playRound();
+  playRound();
+  playRound();
+  playRound();
+  playRound();
+
+  evalScore();
 }
-
-function playGame() {
-
-    humanScore = 0;
-    computerScore = 0;
-
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-
-    evalScore();
-}
-
-
 
 playGame();
