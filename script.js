105e2d4aa5c2bfb3149efec496b9e6588dbceec8
Shreyas KD <149157430+skdoosh@users.noreply.github.com>
2024-09-28 05:59:10 +0530

    Added Pseudocode to the script.js file

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World!")
+// Pseudocode for Rock Paper Scissors project.
+// initialize humanScore and computerScore with 0
+// write code to get computer's choice
+//     create a random whole number between 1 and 3 (inclusive)
+//     map 1, 2, 3 to rock, paper, scissors
+//     return the choice corresponding to the random number
+// write code to get human's choice
+    // Prompt user to enter rock, paper, or scissors
+        // user input validation?
+    // return choice
+// write playround function which takes user choice and computer choice as arguments and then console.logs who won and increments the score counters accordingly
+// wrap everything in the play game function which plays 5 rounds of the game.
+// make sure everything works at each step
+    
